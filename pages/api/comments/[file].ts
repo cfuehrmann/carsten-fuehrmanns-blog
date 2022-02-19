3e03e34fb229bccf342a86569f9c92f4b5f367a8
Carsten Führmann <cfuhrmann@gmail.com>
2022-02-19 21:42:19 +0100

    Add .env.local for comment dir

--- pages/api/comments/[file].ts
+++ pages/api/comments/[file].ts
@@ -4,9 +4,8 @@ import { readComments } from "../../../lib/comments";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { file } = req.query;
   try {
-    const comments = readComments(
-      `/home/carsten/web-site-comments/${file}.json`
-    );
+    const commentDir = process.env["COMMENT_DIR"];
+    const comments = readComments(`${commentDir}${file}.json`);
     res.status(200).json(comments);
   } catch (e) {
     res.status(404).json([]);
